b3aa1394fb67ee9980a3f86361a416d43844a20c
Meriç Mert Bulca <meric.bulca@obss.tech>
2024-10-01 16:40:34 +0300

    first commit

---
+++ src/test/java/com/mericbulca/springbootboilerplate/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mericbulca.springbootboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
